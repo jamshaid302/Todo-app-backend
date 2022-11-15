58a8819b742c6eebb6d9f595622c7e0443927669
Jamshaid khan <jkniazi302@gmail.com>
2022-11-15 21:33:24 +0500

    change in conn and test file

--- utility/conn.js
+++ utility/conn.js
@@ -2,17 +2,16 @@ require('dotenv').config();
 const mongoose = require("mongoose");
 
 // mongoose.connect(process.env.DB_URI.mongodatabaseConnection,{ useNewUrlParser: true,useUnifiedTopology: true});
-
     if(process.env.NODE_ENV == 'development'){
         mongoose.connect(process.env.DB_URI);
         const { connection } = mongoose;
         connection.once("connected", () => console.log("Database Connected ~"))
         connection.on("error", error => console.log("Database Error: ", error))
     }
-    // else {
-    //     // mongoose.connect(`mongodb+srv://jamshaid:jamshaid@cluster0.aycmrpn.mongodb.net/test_userListing`);
-    //     mongoose.connect(process.env.TEST_DB_URI);
-    //     const { connection } = mongoose;
-    //     connection.once("connected", () => console.log("Test Database Connected ~"))
-    //     connection.on("error", error => console.log("Database Error: ", error))
-    // }
+    else {
+        // mongoose.connect(`mongodb+srv://jamshaid:jamshaid@cluster0.aycmrpn.mongodb.net/test_userListing`);
+        mongoose.connect(process.env.TEST_DB_URI);
+        const { connection } = mongoose;
+        connection.once("connected", () => console.log("Test Database Connected ~"))
+        connection.on("error", error => console.log("Database Error: ", error))
+    }
