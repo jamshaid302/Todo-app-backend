2fd554e45843d88bdd509ee706efb0fde850e0d1
Jamshaid khan <jkniazi302@gmail.com>
2022-11-15 15:27:48 +0500

    add github action file

--- utility/conn.js
+++ utility/conn.js
@@ -3,15 +3,15 @@ const mongoose = require("mongoose");
 
 // mongoose.connect(process.env.DB_URI.mongodatabaseConnection,{ useNewUrlParser: true,useUnifiedTopology: true});
 
-    if(process.env.NODE_ENV == 'development'){
+    // if(process.env.NODE_ENV == 'development'){
         mongoose.connect(process.env.DB_URI);
         const { connection } = mongoose;
         connection.once("connected", () => console.log("Database Connected ~"))
         connection.on("error", error => console.log("Database Error: ", error))
-    }
-    else {
-        mongoose.connect(`mongodb+srv://jamshaid:jamshaid@cluster0.aycmrpn.mongodb.net/test_userListing`);
-        const { connection } = mongoose;
-        connection.once("connected", () => console.log("Test Database Connected ~"))
-        connection.on("error", error => console.log("Database Error: ", error))
-    }
+    // }
+    // else {
+    //     mongoose.connect(`mongodb+srv://jamshaid:jamshaid@cluster0.aycmrpn.mongodb.net/test_userListing`);
+    //     const { connection } = mongoose;
+    //     connection.once("connected", () => console.log("Test Database Connected ~"))
+    //     connection.on("error", error => console.log("Database Error: ", error))
+    // }
