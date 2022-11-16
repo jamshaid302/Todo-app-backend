4ed0d5cb92d74048edc709b7abb6602c83ba5897
Jamshaid khan <jkniazi302@gmail.com>
2022-11-16 16:29:27 +0500

    change in package.json file

--- test/list/list.spec.js
+++ test/list/list.spec.js
@@ -3,18 +3,18 @@ const request = require('supertest');
 const app = require('../../server/app');
 
 describe('project list test', () => {
-    // beforeEach( async () => {
-    //     console.log(process.env.DB_URI)
-    //         const db = await mongoose.connect(
-    //             // `mongodb+srv://jamshaid:jamshaid@cluster0.aycmrpn.mongodb.net/test_userListing`
-    //             process.env.TEST_DB_URI
-    //         );
-    //         if(db){
-    //             console.log('Test DB Connection established');
-    //         } else{
-    //             console.log('Test DB Connection error');
-    //         }
-    // })
+    beforeEach( async () => {
+        console.log(process.env.TEST_DB_URI)
+            const db = await mongoose.connect(
+                // `mongodb+srv://jamshaid:jamshaid@cluster0.aycmrpn.mongodb.net/test_userListing`
+                process.env.TEST_DB_URI
+            );
+            if(db){
+                console.log('Test DB Connection established');
+            } else{
+                console.log('Test DB Connection error');
+            }
+    })
 
     it('should add a list', async () => {
         const lists = await request(app)
--- utility/conn.js
+++ utility/conn.js
@@ -8,10 +8,10 @@ const mongoose = require("mongoose");
         connection.once("connected", () => console.log("Database Connected ~"))
         connection.on("error", error => console.log("Database Error: ", error))
     }
-    else {
-        // mongoose.connect(`mongodb+srv://jamshaid:jamshaid@cluster0.aycmrpn.mongodb.net/test_userListing`);
-        mongoose.connect(process.env.TEST_DB_URI);
-        const { connection } = mongoose;
-        connection.once("connected", () => console.log("Test Database Connected ~"))
-        connection.on("error", error => console.log("Database Error: ", error))
-    }
+    // else {
+    //     // mongoose.connect(`mongodb+srv://jamshaid:jamshaid@cluster0.aycmrpn.mongodb.net/test_userListing`);
+    //     mongoose.connect(process.env.TEST_DB_URI);
+    //     const { connection } = mongoose;
+    //     connection.once("connected", () => console.log("Test Database Connected ~"))
+    //     connection.on("error", error => console.log("Database Error: ", error))
+    // }
